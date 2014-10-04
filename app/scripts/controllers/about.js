b270ee37bfa9b44bf0c857390cb0b75fdaf958dc
Daniel Kennedy <danwkennedy@gmail.com>
2014-10-04 15:04:52 -0400

    Initial generation commit.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cellularAutomationApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cellularAutomationApp
+ */
+angular.module('cellularAutomationApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
